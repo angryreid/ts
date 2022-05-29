a2fc828b6d0b14fdd90a61e5d9a41b1b9d2833b0
angryreid <onyourwill@gmail.com>
2022-05-29 14:57:18 +0800

    Feature: DI init

---
+++ learn/03-dependency-injection/drawback.ts
@@ -0,0 +1,27 @@
+class Engine {
+  constructor() {}
+}
+
+class Tire {
+  constructor() {}
+}
+
+class NewEngine extends Engine {
+  enhancement: string;
+
+  constructor(enhancement: string) {
+    super();
+    this.enhancement = enhancement;
+  }
+}
+
+class Car {
+  engine: Engine;
+  tire: Tire;
+
+  constructor() {
+    // this.engine = new Engine();
+    this.engine = new NewEngine();// If the engine enhanced, the car will be broken.
+    this.tire = new Tire();
+  }
+}
